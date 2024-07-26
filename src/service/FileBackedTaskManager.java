5f3e0bedded9d220d130840b59671ff3e778244f
eccentric <cool.eccentric@yandex.ru>
2024-07-26 16:21:50 +0300

    --amend

--- src/service/FileBackedTaskManager.java
+++ src/service/FileBackedTaskManager.java
@@ -1,17 +1,36 @@
 package service;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileBackedTaskManager extends InMemoryTaskManager {
 
-    private final Path filePath;
-
-    public FileBackedTaskManager(HistoryManager historyManager, Path filePath) {
+    public FileBackedTaskManager(HistoryManager historyManager) {
         super(historyManager);
-        this.filePath = filePath;
     }
 
-    private void save() {
+    public void createFile() throws Exception {
+        try {
+            Path direct = Paths.get("C:\\yp\\testingfiles");
+
+            if (!Files.exists(direct)) {
+                Files.createDirectories(direct);
+            }
+
+            Path file = direct.resolve("test.csv");
+
+            if (!Files.exists(file)) {
+                Files.createFile(file);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Не удалось создать директорию или файл", e);
+        }
+    }
+
+    private void save() throws IOException {
 
     }
 
